e40d074880dd6668bb843bead81170ac20782933
prakv <prakv@ebay.com>
2015-11-29 03:12:07 +0530

    adding missing tags

---
+++ tags/common/head.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+<title><?php echo $_TITLE?></title>
+<link href="resources/css/bootstrap.css" rel="stylesheet">
+<link href="resources/css/responsive.css" rel="stylesheet">
+<link href="resources/css/style.css" rel="stylesheet">
+<link href="resources/css/pages.css" rel="stylesheet">
