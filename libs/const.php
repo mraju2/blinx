6c49ed18f6bac904f086f7204a4d7d87bd476f9a
prakv <prakv@ebay.com>
2015-11-29 02:35:08 +0530

    libs

---
+++ libs/const.php
@@ -0,0 +1,15 @@
+<?php
+
+define("URL_HOME", "index.php");
+define("URL_SEARCH", "search.php");
+define("URL_REQUEST", "request.php");
+//
+define("URL_ABOUT", "about.php");
+define("URL_HOW_IT_WORK", "howitworks.php");
+define("URL_CONTACT", "contact.php");
+define("URL_SERVICE", "services.php");
+//
+define("URL_SIGNIN", "signin.php");
+define("URL_JOIN", "join.php");
+
+
