f564c9dcfcd2aba6d86653058deeb270d6ad9252
Prabin Patodia <ppatodia@paypal.com>
2015-11-29 06:31:28 +0530

    changes in services

--- tags/home/service.php
+++ tags/home/service.php
@@ -14,8 +14,10 @@
                     <div class="featured-photos">
                         <i class="fa fa-gift"></i>
                     </div>
-                    <h3>Service 1</h3>
-                    <p>Short description of service comes here.Short description of service comes here.Short description of service comes here.</p>
+                    <h3>Become a sighted volunteer</h3>
+                        <p>
+                            Blinx could not exist without sighted volunteers. If you have time.
+                        </p>
                 </div>
             </div>
             <div class="col-md-4 col-sm-4">
@@ -23,8 +25,10 @@
                     <div class="featured-photos">
                         <i class="fa fa-heart-o"></i>
                     </div>
-                    <h3>Service 2</h3>
-                    <p>Short description of service comes here.Short description of service comes here.Short description of service comes here.</p>
+                    <h3>Donate to the project</h3>
+                        <p>
+                            If you want to help us reach millions of people, who are estimated to be visually impaired worldwide, you can donate to the project.
+                        </p>
                 </div>
             </div>
             <div class="col-md-4 col-sm-4">
@@ -32,8 +36,10 @@
                     <div class="featured-photos">
                         <i class="fa fa-flag"></i>
                     </div>
-                    <h3>Service 3</h3>
-                    <p>Short description of service comes here.Short description of service comes here.Short description of service comes here.</p>
+                    <h3>Register to ask for a help</h3>
+                        <p>
+                            Blinx could not exist without people asking for help. If you need help register TODAY! Its free.
+                        </p>
                 </div>
             </div>
             <div class="col-md-12 col-sm-12">
